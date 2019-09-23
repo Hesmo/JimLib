2e7e3839ede43eefaf899dadc48f61a370c1da38
VmDebian Lorge <jeanmarc.ghanassia@lorge.fr>
2019-09-23 09:45:27 +0200

    Ajout HTML5_comment

--- lib-html5.php
+++ lib-html5.php
@@ -227,5 +227,7 @@ function HTML5_StopCache() {
 	header('Cache-Control: no-cache, must-revalidate');
 	header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date dans le passé
 }
-
+function HTML5_comment($commentaire){
+	echo "<!-- ${commentaire} -->";
+}
 ?>
